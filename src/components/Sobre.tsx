7be728595d5db67b10445301a7c7b2bcec881dc0
Akiraaa07 <igorakira2004@gmail.com>
2024-04-01 16:21:00 -0300

    Decimo commit add link do github - Nicola

--- src/components/Sobre.tsx
+++ src/components/Sobre.tsx
@@ -8,6 +8,7 @@ const SobrePage: React.FC = () => {
                     <h1 className='text-blue-900 text-center text-3xl py-16'>Equipe</h1>
                     <p className='text-center py-8'>Igor Akira Bortolini Tateishi RM: 554227</p>
                     <p className='text-center py-8'>Nicola Monte Cravo Garofalo RM: 553991</p>
+                    <p className='text-center py-8'>Link do respositorio GitHub: https://github.com/Akiraaa07/Sprint-3-FrontEnd</p>
                 </aside>
             </main>
         </div>
